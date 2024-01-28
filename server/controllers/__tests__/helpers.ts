c90de7a6f808f9394e51f5a095b0762b5deab634
Andrew Dame <67058362+Andrew-Dame@users.noreply.github.com>
2024-01-27 19:01:34 -0500

    feat: Removes api code and adds api as a git submodule (#41)

--- server/controllers/__tests__/helpers.ts
+++
@@ -1,8 +0,0 @@
-export const setTestEnvironmentVars = () => {
-  process.env.APP_ENV = "development";
-  process.env.AUTH_SECRET = "foo";
-  process.env.DATABASE_URL = "postgres://test-user:test-pw@foo:5432/bar";
-  process.env.EMAIL_ADDRESS = "test@foobar.com";
-  process.env.EMAIL_PASSWORD = "bar";
-  process.env.EMAIL_PROVIDER = "Zoho";
-}
