717c723af94478525b1a7b4ae9cc03ee7ffb8f21
Santiago Verdu <erra@qmake.org>
2024-09-10 20:06:04 -0600

    remove terraform

--- cloudwatch.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_cloudwatch_log_group" "ecs_log_group" {
-  name              = "/ecs/mqm-scorecard"
-  retention_in_days = 7
-}
--- secrets.tf
+++
@@ -1,3 +0,0 @@
-data "aws_secretsmanager_secret_version" "db_password_version" {
-  secret_id = "prod-database"
-}
